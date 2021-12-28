c3dc3a287c4e8372d814c050028aae1dac2bd51f
harryuan65 <harryuan.65@gmail.com>
2021-12-28 13:40:08 +0800

    feat: mail on comment create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @post.comments.create! params.required(:comment).permit(:content) # magic
+    comment = @post.comments.create! params.required(:comment).permit(:content) # magic
+    CommentsMailer.submitted(comment).deliver_later # will use async job queue. Q: what is the default queue?
     redirect_to @post
   end
 
