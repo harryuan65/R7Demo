d4757b8c3e0b4c04742cb3b98e28fa9e6d3d1b96
harryuan65 <harryuan.65@gmail.com>
2021-12-28 15:52:57 +0800

    feat: allow rails console modification to apply in realtime

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+  validates :body, presence: true
+
+  after_create_commit { broadcast_prepend_to 'tweets' }
+  after_update_commit { broadcast_replace_to 'tweet' }
+  after_destroy_commit { broadcast_remove_to 'tweet' }
 end
